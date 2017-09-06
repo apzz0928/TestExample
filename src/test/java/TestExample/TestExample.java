cc5ad0a8f3dcba596140a0f793905552ce5f4d69
apzz0928 <apzz0928@gmail.com>
2017-09-06 18:45:36 +0900

    selenide + Grid 예제 적용

--- src/test/java/TestExample/TestExample.java
+++ src/test/java/TestExample/TestExample.java
@@ -1,20 +1,45 @@
 package TestExample;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import com.codeborne.selenide.WebDriverRunner;
+
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
 public class TestExample {		
+	private static String baseUrl, nodeURL;
+	
 	@BeforeClass
-	public void beforeTest() {
+	public void beforeTest() throws MalformedURLException {
 		System.setProperty("webdriver.chrome.driver", "D:/Selenium/driver/chromedriver.exe");
-		System.setProperty("selenide.browser", "Chrome");	        
+		System.setProperty("selenide.browser", "Chrome");	 
+		System.setProperty("selenide.browser", "Chrome");
+			baseUrl = "https://nid.naver.com/nidlogin.login";
+	        nodeURL = "http://10.10.105.228:5555/wd/hub";
+	        
+	        String urlToRemoteWD = "http://10.10.105.228:4444/wd/hub";
+	        RemoteWebDriver driver =new RemoteWebDriver(new URL(urlToRemoteWD),DesiredCapabilities.chrome());
+	        WebDriverRunner.setWebDriver(driver);
+	        
+	        /*
+	    	DesiredCapabilities cap = DesiredCapabilities.chrome();
+			cap.setBrowserName("chrome");
+			cap.setPlatform(Platform.WINDOWS);
+			driver = new RemoteWebDriver(new URL(nodeURL), cap);
+			*/
     }
 	@Test
 	public void Login() throws Exception {
-        open("https://nid.naver.com/nidlogin.login");
+        open(baseUrl);
         Thread.sleep(3000);
         System.out.println("------------------start-----------------");
         $("#id").setValue("apzz0928");
