242ab1651ebea08196ac7ad2ef902cf38613652a
apzz0928 <apzz0928@gmail.com>
2017-09-06 18:49:50 +0900

    selenide + Grid 예제 적용 (Grid 사용하니 Jenkins에서도 테스트 진행됨)

--- src/test/java/TestExample/TestExample.java
+++ src/test/java/TestExample/TestExample.java
@@ -3,7 +3,6 @@ package TestExample;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
@@ -16,26 +15,17 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
 public class TestExample {		
-	private static String baseUrl, nodeURL;
+	private static String baseUrl;
 	
 	@BeforeClass
 	public void beforeTest() throws MalformedURLException {
 		System.setProperty("webdriver.chrome.driver", "D:/Selenium/driver/chromedriver.exe");
 		System.setProperty("selenide.browser", "Chrome");	 
 		System.setProperty("selenide.browser", "Chrome");
-			baseUrl = "https://nid.naver.com/nidlogin.login";
-	        nodeURL = "http://10.10.105.228:5555/wd/hub";
-	        
-	        String urlToRemoteWD = "http://10.10.105.228:4444/wd/hub";
-	        RemoteWebDriver driver =new RemoteWebDriver(new URL(urlToRemoteWD),DesiredCapabilities.chrome());
-	        WebDriverRunner.setWebDriver(driver);
-	        
-	        /*
-	    	DesiredCapabilities cap = DesiredCapabilities.chrome();
-			cap.setBrowserName("chrome");
-			cap.setPlatform(Platform.WINDOWS);
-			driver = new RemoteWebDriver(new URL(nodeURL), cap);
-			*/
+		baseUrl = "https://nid.naver.com/nidlogin.login";
+        String urlToRemoteWD = "http://10.10.105.228:4444/wd/hub";
+        RemoteWebDriver driver =new RemoteWebDriver(new URL(urlToRemoteWD),DesiredCapabilities.chrome());
+        WebDriverRunner.setWebDriver(driver);
     }
 	@Test
 	public void Login() throws Exception {
