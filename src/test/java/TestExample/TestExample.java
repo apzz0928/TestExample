85695bed817ee142ca8f4fed5b636d2b7cfb8f32
apzz0928 <apzz0928@gmail.com>
2017-09-07 15:18:17 +0900

    샘플소스 위메프 로그인으로 변경

--- src/test/java/TestExample/TestExample.java
+++ src/test/java/TestExample/TestExample.java
@@ -3,6 +3,7 @@ package TestExample;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
@@ -14,6 +15,8 @@ import com.codeborne.selenide.WebDriverRunner;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
+import com.codeborne.selenide.testng.ScreenShooter;
+
 public class TestExample {		
 	private static String baseUrl;
 	
@@ -22,23 +25,23 @@ public class TestExample {
 		System.setProperty("webdriver.chrome.driver", "D:/Selenium/driver/chromedriver.exe");
 		System.setProperty("selenide.browser", "Chrome");	 
 		System.setProperty("selenide.browser", "Chrome");
-		baseUrl = "https://nid.naver.com/nidlogin.login";
+		baseUrl = "http://wemakeprice.com/";
         String urlToRemoteWD = "http://10.10.105.228:4444/wd/hub";
         RemoteWebDriver driver =new RemoteWebDriver(new URL(urlToRemoteWD),DesiredCapabilities.chrome());
         WebDriverRunner.setWebDriver(driver);
+        ScreenShooter.captureSuccessfulTests = true;
     }
 	@Test
-	public void Login() throws Exception {
+	public void Login() throws InterruptedException{
         open(baseUrl);
-        Thread.sleep(3000);
-        System.out.println("------------------start-----------------");
-        $("#id").setValue("apzz0928");
-        $("#pw").setValue("qordlf!@34");
-        $(".btn_global").click();
-        Thread.sleep(3000);
-        $("#query").val("selenide");
-        $("#search_btn").click();
-        System.out.println("------------------end-----------------");
+        $("#loginBtn").click();
+        $("#login_uid").setValue("apzz0928");
+        $("#login_upw").setValue("qordlf!@34");
+        $("#loginConfirmBtn").click();
+        $("#pw_next_view").click();
+        $(".customer").hover();
+        $("#LogoutBtn").click(); 
+        Thread.sleep(2000);
     }
 	@AfterClass
 	public void afterTest() {
