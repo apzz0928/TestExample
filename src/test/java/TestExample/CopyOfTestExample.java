20f64d2b7979ee0175deaed499e87dc91bb54242
apzz0928 <apzz0928@gmail.com>
2017-09-18 16:08:33 +0900

    2017/09/18

--- src/test/java/TestExample/CopyOfTestExample.java
+++ src/test/java/TestExample/CopyOfTestExample.java
@@ -3,7 +3,7 @@ package TestExample;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
@@ -13,6 +13,7 @@ import org.testng.annotations.Test;
 import com.codeborne.selenide.WebDriverRunner;
 
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
 import com.codeborne.selenide.testng.ScreenShooter;
@@ -24,7 +25,7 @@ public class CopyOfTestExample {
 	public void beforeTest() throws MalformedURLException {
 		System.setProperty("webdriver.chrome.driver", "D:/Selenium/driver/chromedriver.exe");
 		System.setProperty("selenide.browser", "Chrome");
-		baseUrl = "https://nid.naver.com/nidlogin.login";
+		baseUrl = "http://www.nexon.com/Home/Game.aspx";
         String urlToRemoteWD = "http://10.10.105.228:4444/wd/hub";
         RemoteWebDriver driver =new RemoteWebDriver(new URL(urlToRemoteWD),DesiredCapabilities.chrome());
         WebDriverRunner.setWebDriver(driver);
@@ -33,15 +34,13 @@ public class CopyOfTestExample {
 	@Test
 	public void Login() throws InterruptedException{
         open(baseUrl);
-        $("#id").setValue("apzz0928");
-        $("#pw").setValue("qordlf!234");
-        $(".btn_global").click();
-        Thread.sleep(2000);
-        $("#query").setValue("Selenide");
-        $(".ico_search_submit").click();
-        Thread.sleep(2000);
-        $(".sh_blog_title").click();
-        Thread.sleep(2000);
+        $(By.linkText("ID 로그인")).click();
+        $("#txtNexonID").setValue("apzz0928@gmail.com");
+        $("#txtPWD").setValue("qordlf!@34");
+        $("#btnLogin").click();
+        $("img[alt='다음에하기'").click();
+        $(".button01").click();
+        $(".join").waitUntil(text("회원가입"), 3000);
     }
 	@AfterClass
 	public void afterTest() {
